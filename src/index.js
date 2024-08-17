db929248ea8a3738573888c49b1b49faa3f63f07
solowihmaria <solowihmaria@gmail.com>
2024-08-17 17:31:55 +0300

    feat: add towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
 
-// You should implement your task here.
+  const result = [];
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+ // так как там идёт змейкой, проверим на четность и нечетность строк матрицы
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      // для четных строк обычный порядок
+      result.push(...matrix[i]); 
+    } else {
+      // для нечетных строк переворот строки
+      result.push(...matrix[i].reverse());
+    }
+  }
+
+  return result;
+};
